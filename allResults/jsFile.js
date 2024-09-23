dec335a0ed60e14fbf3d4b58f632cd8231f0497b
Kirtesh Wani <wanikirtesh@gmail.com>
2024-09-23 10:52:23 +0530

    cleanup

--- allResults/jsFile.js
+++
@@ -1,7 +0,0 @@
-results.push('results/result_1.json');
-results.push('results/result_.json');
-results.push('results/result_20240920053524.json');
-results.push('results/result_20240920114445.json');
-results.push('results/result_20240920123557.json');
-results.push('results/result_20240920123750.json');
-results.push('results/result_20240923045859.json');
