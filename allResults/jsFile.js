ac95a23d190053e4698bfb171a36c013ca6e1b3b
Kirtesh Wani <wanikirtesh@gmail.com>
2024-10-17 18:55:12 +0530

    Update jsFile.js

--- allResults/jsFile.js
+++ allResults/jsFile.js
@@ -7,4 +7,4 @@ results.push({'timestamp':'20241010125315','tag':'Something else'});
 results.push({'timestamp':'20241011054754','tag':'Regression'});
 results.push({'timestamp':'20241011090427','tag':'linuxDB'});
 results.push({'timestamp':'20241011150253','tag':'new Demo'});
-results.push({'timestamp':'20241017131415','tag':'MyLatestRun'});
+results.push({'timestamp':'20241017131415','tag':'Regression'});
